b8217988c6045b999d4bec282c69f017e6946bab
Adam Vendrasco <adamvendrasco@trout-kryqm.device.utk.edu>
2024-04-16 14:14:57 -0400

    testing stuff

--- underlyingEvent.py
+++ underlyingEvent.py
@@ -2,8 +2,7 @@ import ROOT
 import uproot
 from ROOT import RDataFrame
 import numpy as np
-from root_pandas import read_root
-
+imprt pandas as pd
 
 dataFile = ROOT.TFile.Open("root://eospublic.cern.ch//eos/opendata/cms/derived-data/PFNano/29-Feb-24/SingleMuon/Run2016G-UL2016_MiniAODv2_PFNanoAODv1/240207_205649/0000/nano_data2016_1.root")
 
