b0194e4ab67b73fe89033701f55ef7709136d29b
Adam Vendrasco <adamvendrasco@trout-kryqm.device.utk.edu>
2024-04-17 10:16:01 -0400

    blahh

--- uproot_test.py
+++ uproot_test.py
@@ -3,6 +3,8 @@ import awkward as ak
 import numpy as np
 import ROOT
 from ROOT import RDataFrame
+import pandas
+import awkward-pandas
 
 print("check1")
 data= uproot.open('root://eospublic.cern.ch//eos/opendata/cms/derived-data/PFNano/29-Feb-24/SingleMuon/Run2016G-UL2016_MiniAODv2_PFNanoAODv1/240207_205649/0000/nano_data2016_1.root')
@@ -23,4 +25,7 @@ print("check5")
 
 
 #Look into using pandas?#Currently getting runtime errors
-PFCan_dz_panda = tree.arrays(filter_name = "PFCands_mass",library = "pd")
+PFak = tree.arrays(filter_name = "PFCands_mass",library="ak")
+print(PFak)
+PFpd=tree.arrays(filter_name= "PFCands_mass",library="pd")
+print(PFpd)
