abf8d0473afd8d4e36081b955fd03533999d0cb8
Adam Vendrasco <adamvendrasco@trout-kryqm.device.utk.edu>
2024-05-06 13:18:26 -0400

    test2 script added

---
+++ test2.py
@@ -0,0 +1,99 @@
+import uproot
+import numpy as np
+from ROOT import TLorentzVector
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Open and read data from the ROOT file
+file = uproot.open("/nfs/home/avendras/nano_data2016_1.root")
+tree = file["Events"]
+data = tree.arrays(["PFCands_pdgId", "PFCands_pt", "PFCands_eta", "PFCands_phi", "PFCands_mass"], 
+        library="np")
+
+# Extract data as individual numpy arrays
+pdgId_array = data["PFCands_pdgId"]
+pt_array = data["PFCands_pt"]
+eta_array = data["PFCands_eta"]
+phi_array = data["PFCands_phi"]
+mass_array = data["PFCands_mass"]
+
+# Initialize lists for different types of particles
+combined_muon_pz_array=[]
+combined_non_muon_pz_array=[]
+
+# Process each event
+for pdgIds, pts, etas, phis, masses in zip(pdgId_array, pt_array, eta_array, phi_array, mass_array):
+    muon_count = 0
+    for pdgId, pt, eta in zip(pdgIds, pts, etas):
+        if abs(pdgId) == 13 and pt > 2.0 and abs(eta) < 2.5:
+            muon_count += 1
+    if muon_count == 2:
+        total_pz_non_muons = 0
+        total_pz_muon = 0
+        for pdgId, pt, eta, phi, mass in zip(pdgIds, pts, etas, phis, masses):
+            if abs(pdgId) ==13:
+                tlv_muon = TLorentzVector()
+                tlv_muon.SetPtEtaPhiM(pt, eta, phi, mass)
+                total_pz_muon += tlv_muon.Pz()
+                combined_muon_pz_array.append(total_pz_muon)
+            
+            if abs(pdgId) != 13:
+                tlv_non_muon = TLorentzVector()
+                tlv_non_muon.SetPtEtaPhiM(pt, eta, phi, mass)
+                total_pz_non_muons += tlv_non_muon.Pz()
+                combined_non_muon_pz_array.append(total_pz_non_muons)
+
+
+#The combined muon pz are my labels!
+combined_muon_pz_array= np.array(combined_muon_pz_array)
+combined_non_muon_pz_array=np.array(combined_muon_pz_array)
+
+print(combined_muon_pz_array.shape)
+print(combined_non_muon_pz_array.shape)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(combined_non_muon_pz_array, combined_muon_pz_array, test_size=0.2, random_state=42)
+
+# Define and compile a neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1000, activation='relu', input_shape=(1,)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(500, activation='relu', input_shape=(1,)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(30, activation='relu', input_shape=(1,)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(50, activation='relu', input_shape=(1,)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1)
+])
+
+# Compile the model with custom linear regression loss function
+model.compile(optimizer='adam', loss="mean_squared_error")
+# Train the model
+history = model.fit(X_train, y_train, epochs=100, batch_size=100, verbose=2)
+# Evaluate the model on the test set
+mse = model.evaluate(X_test, y_test, verbose=0)
+print("Mean Squared Error on Test Set:", mse)
+
+train_loss = history.history['loss']
+y_pred = model.predict(X_test)
+
+
+# Visualize Predictions of the model
+plt.figure()
+plt.scatter(y_test, y_pred)
+plt.xlabel('True Labels')
+plt.ylabel('Predicted Labels')
+plt.title('True vs. Predicted Labels')
+plt.savefig("prediction.png")
+
+# Visualize loss function per epochs
+plt.figure()
+plt.plot(train_loss, label='Training Loss')
+plt.title('Model Loss During Training')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.savefig('model_losses.png')
+
