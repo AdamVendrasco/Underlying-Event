533ee1393f50a2cd473eba5911e96f77cfab8e9c
Adam Vendrasco <avendras@vols.utk.edu>
2025-04-10 08:18:51 -0400

    updated entry.sh to run preProcess code

--- entry.sh
+++ entry.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python /app/Underlying-Event/main_scripts/underlying_event_main_NEWDATA.py
-
+python /app/Underlying-Event/main_scripts//app/Underlying-Event/main_scripts/underlying_event_main_preProcess.py | tee UE30.txt
 
 
