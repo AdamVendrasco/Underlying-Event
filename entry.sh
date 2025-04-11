b2cb6748685037c01b81f000d029748869688aa2
Adam Vendrasco <avendras@vols.utk.edu>
2025-04-11 08:39:11 -0400

    docker update

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python /app/Underlying-Event/main_scripts//app/Underlying-Event/main_scripts/underlying_event_main_preProcess.py | tee UE30.txt
+python /app/Underlying-Event/main_scripts/underlying_event_main_preProcess.py | tee UE30.txt
 
 
